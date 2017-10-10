88ade3ee7639c80d9af9423c7bb55712b0c74553
Hal Gatewood <hal@halgatewood.com>
2017-10-10 11:01:43 -0500

    Update _vars.php

--- lib/_vars.php
+++ lib/_vars.php
@@ -1,7 +1,7 @@
 <?php
 	
-$use_db				= false;
+$use_db			= false;
 $index_fallback 	= true;
 $home_module	 	= "home";
 $login_module		= "login";
-$fof_text			= "<h1>Error</h1><p>This page couldn't be found on our system. Please try again.</p>";
+$fof_text		= "<h1>Error</h1><p>This page couldn't be found on our system. Please try again.</p>";
