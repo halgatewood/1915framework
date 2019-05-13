43c030ff7cc1eec8b0a62d0069f9c599305f134d
Hal Gatewood <hal@halgatewood.com>
2019-05-13 13:07:00 -0500

    cleanup

--- lib/_vars.php
+++ lib/_vars.php
@@ -2,6 +2,6 @@
 	
 $use_db			= false;
 $index_fallback 	= true;
-$home_module	 	= "home";
-$login_module		= "login";
+$home_module	 	= 'home';
+$login_module		= 'login';
 $fof_text		= "<h1>Error</h1><p>This page couldn't be found on our system. Please try again.</p>";
