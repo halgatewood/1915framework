ece34f082a7935edf540256c562fe3c14dd3ad52
Hal Gatewood <hal@halgatewood.com>
2019-05-13 13:05:31 -0500

    cleanup on aisle 1

--- lib/application.php
+++ lib/application.php
@@ -2,31 +2,31 @@
 	
 	
 // SYSTEM VARS
-$now 				= time();
-$ip 				= $_SERVER['REMOTE_ADDR']; 
+$now 			= time();
+$ip 			= $_SERVER['REMOTE_ADDR']; 
 $request_uri 		= $_SERVER['REQUEST_URI'];
-$uri  				= explode("/", $request_uri);
-$is_local 			= strpos($_SERVER['HTTP_HOST'], ".dev") !== false ? true : false;
+$uri  			= explode('/', $request_uri);
+$is_local 		= strpos($_SERVER['HTTP_HOST'], '.test') !== false ? true : false;
 $body_class 		= array();
 
 
 // LIB
-require_once(dirname(__FILE__)."/_vars.php");
-require_once(dirname(__FILE__)."/dbug.php");
-require_once(dirname(__FILE__)."/modules.php");
+require_once(dirname(__FILE__).'/_vars.php');
+require_once(dirname(__FILE__).'/dbug.php');
+require_once(dirname(__FILE__).'/modules.php');
 
 
 // DATABASE
 if( $use_db ) 
 {
-	require_once(dirname(__FILE__)."/database.php");
-	require_once(dirname(__FILE__)."/mysql.php");
+	require_once(dirname(__FILE__).'/database.php');
+	require_once(dirname(__FILE__).'/mysql.php');
 	if( isset($db_params) ) $db = new mysql( $db_params );
 }
 
 
 // FUNCTIONS
-require_once(dirname(__FILE__)."/functions.php");
+require_once(dirname(__FILE__).'/functions.php');
 
 
 // MODULES & ACTIONS
@@ -43,22 +43,22 @@ if( $id ) $body_class[] = "id-$id";
 
 
 // CHECK SECURE
-if( in_array($module, (array) $secure_modules) AND !is_user_logged_in()) redirect("/" . $login_module);
+if( in_array($module, (array) $secure_modules) AND !is_user_logged_in()) redirect('/' . $login_module);
 
 
 // GLOBAL ACTIONS
-require_once(dirname(__FILE__)."/global.php");
+require_once(dirname(__FILE__).'/global.php');
 
 
 // MODULE INDEX FALLBACK
-if( $index_fallbacks AND $index_fallbacks[$module] !== "") $index_fallback = $index_fallbacks[$module];
+if( $index_fallbacks AND $index_fallbacks[$module] !== '') $index_fallback = $index_fallbacks[$module];
 
 
 // ACTION
 $has_action = false;
-if(is_dir(dirname(__FILE__)."/../modules/$module"))
+if(is_dir(dirname(__FILE__). '/../modules/$module'))
 {
-	if(file_exists(dirname(__FILE__)."/../modules/$module/a.$action.php"))
+	if(file_exists(dirname(__FILE__). "/../modules/$module/a.$action.php"))
 	{
 		require_once(dirname(__FILE__)."/../modules/$module/a.$action.php");
 		$has_action = true;
@@ -104,9 +104,9 @@ else
 // 404
 if( $throw_404 ) 
 {
-	if(!headers_sent()) { header("HTTP/1.0 404 Not Found"); }
+	if(!headers_sent()) { header('HTTP/1.0 404 Not Found'); }
 	$view = $fof_text;
-	$body_class[] = "error-404";
+	$body_class[] = 'error-404';
 }
 
 
@@ -114,17 +114,17 @@ if( $throw_404 )
 if( is_ajax() )
 {
 	$has_layout = false;
-	require_once(dirname(__FILE__) . "/../templates/blank.php");
+	require_once(dirname(__FILE__).'/../templates/blank.php');
 	$has_layout = true;
 }
 else
 {
 	$has_layout = false;
 	if( isset($templates[$module]) ) $template = $templates[$module];
-	if(file_exists(dirname(__FILE__) . "/../templates/" . $template . ".php"))
+	if(file_exists(dirname(__FILE__) . '/../templates/' . $template . '.php'))
 	{
 		$body_class[] = "template-$template";
-		require_once(dirname(__FILE__) . "/../templates/" . $template . ".php");
+		require_once(dirname(__FILE__).'/../templates/' . $template . '.php');
 		$has_layout = true;
 	}
 }
